2a0a58d6e9ffdabcd29131d9ba26d70ef09abc8a
victory-nwawunze <victorynwawunze@gmail.com>
2023-03-07 14:33:11 +0100

    Classes in dart

---
+++ classes.dart
@@ -0,0 +1,23 @@
+void main() {
+  Person p1 = Person("Angel", "Male", 2);
+  p1.setData();
+}
+
+class Person {
+  String? name;
+  String? sex;
+  int? age;
+
+  // Constructor
+  Person(String name, String sex, int age) {
+    this.name = name;
+    this.age = age;
+    this.sex = sex;
+  }
+  void setData() {
+    print("name ========> $name");
+    print("age ======> $age");
+    print("sex ======> $sex");
+    print("my name is $name and i am $age yrs old i am a $sex");
+  }
+}
